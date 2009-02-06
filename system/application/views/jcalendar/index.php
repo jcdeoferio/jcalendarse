2fbade657855cc5ed8113c8b809eb934b0d6cf47
cedualino200 <cedualino200@1b50f3e4-f447-11dd-a320-db7d8e7a638f>
2009-02-06 12:25:37 +0000

    initial import

---
+++ system/application/views/jcalendar/index.php
@@ -0,0 +1,37 @@
+<h2>EVENTS</h2>
+<table border=1 cellpadding=4>
+  <tr>
+    <th>Event Name</th>
+    <th>Start Date</th>
+    <th>End Date</th>
+    <th>Action</th>
+  </tr>
+  <?php foreach ($events as $event): ?>
+  <tr>
+    <td><?= $event['eventname'] ?></td>
+    <td><?= $event['start_date'] ?></td>
+    <td><?= $event['end_date'] ?></td>
+    <td><?= anchor('jcalendar2/update/'. $event['eventid'], 'update') . '|' . anchor('jcalendar2/delete/' . $event['eventid'], 'delete', array('onClick'=>"return (confirm('Are you sure you want to delete this event?'))")) ?></td>
+  </tr>
+  <?php endforeach; ?>
+</table>
+<br/>
+<?= form_open('jcalendar2/index') ?>
+<!--View queries<br/>
+<table>
+  <tr>
+    <td>starting from:</td>
+    <td><?= form_dropdown('start_month', array(''=>'') + months_array()) ?><?= form_dropdown('start_year', array(''=>'') + years_array()) ?></td>
+  </tr>
+  <tr>
+    <td>ending at:</td>
+    <td><?= form_dropdown('end_month', array(''=>'') + months_array()) ?><?= form_dropdown('end_year', array(''=>'') + years_array()) ?></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td><?= form_submit('submit', 'Go') ?></td>
+  </tr>
+</table>
+<?= form_close() ?>-->
+<br/>
+<?= anchor('jcalendar2/add', 'add event') . '|' . anchor('login/logout', 'logout') ?>
---
+++ system/application/views/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+  <title>JCalendar - <?= $title ?></title>
+  <link rel="shortcut icon" href="http://localhost/files/favicon.png" type="image/x-icon" />
+</head>
+
+<body>
+  <?= img('files/logo-small.png') ?>
+  <hr/>
+  <!--<marquee><h1>JuanCalendar</h1></marquee> -->
+  <?= $body ?>
+</body>
+
+</html>
