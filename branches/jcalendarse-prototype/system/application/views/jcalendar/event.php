fe476c486a13fc7e25860474a1d7d4c031d66c5d
cedualino200 <cedualino200@1b50f3e4-f447-11dd-a320-db7d8e7a638f>
2009-02-07 15:33:51 +0000

    forgot to add the event view earlier

---
+++ branches/jcalendarse-prototype/system/application/views/jcalendar/event.php
@@ -0,0 +1,5 @@
+<h1>Event: <?= $event['eventname'] ?></h1>
+<?= $event['start_date'] . '<br/>' ?>
+<?= $event['end_date'] . '<br/>' ?>
+<?= $event['eventdetails'] . '<br/>' ?>
+<?= $event['venue_name'] . '<br/>' ?>
