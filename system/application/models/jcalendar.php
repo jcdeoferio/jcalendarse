e6723b9ab1d6274c3c3b1847bd98183829cc42d9
cedualino200 <cedualino200@1b50f3e4-f447-11dd-a320-db7d8e7a638f>
2009-02-09 01:11:34 +0000

    updated getting user from rss data. also, testing if the actual files get uploaded or just the symbolic links when sym links are committed into the repository

--- system/application/models/jcalendar.php
+++ system/application/models/jcalendar.php
@@ -32,45 +32,12 @@ class JCalendar extends Model{
     return($this->select_events_by_criteria($userid, null, null, null, null));
   }
       
-  function select_events_by_name($userid, $name){
-    return ($this->select_events_by_criteria($userid, $name, null, null, null));
-  }
-
-  function select_events_by_range($userid, $start_date, $end_date){
-    return($this->select_events_by_criteria($userid, null, $start_date, $end_date, null));
-  }
-
-  function select_events_by_venue($userid, $venue){
-    return($this->select_events_by_criteria($userid, null, null, null, $venue));
-  }
-
-  function check_rss_data($data){
+  function get_user_from_rss_data($data){
     $this->db->from('users');
     $this->db->where('md5(login || password)', $data);
 
-    return($this->db->count_all_results() > 0);
-  }
-
-  function check_permissions_rss($eventid, $data){
-    $this->db->from('users');
-    $this->db->where('md5(login || password)', $data);
-	
-    $user = $this->db->get();
-    $user = $result->row_array();
-	
-    return($this->check_permissions($eventid, $result['userid']));
-  }
-
-  function check_permissions($eventid, $userid){
-    //events with their permissions X group membership table
-    $this->db->from('events inner join permissions p using (eventid), member_of m');
-    //selecting from that table
-    $this->db->where('p.groupid = m.groupid or p.userid = m.userid');
-    $this->db->where('eventid', $eventid);
-    //does this user have permissions
-    $this->db->where('m.userid', $userid);
-	
-    return($this->db->count_all_results() > 0);
+    $query = $this->db->get();
+    return($query->row_array());
   }
 
   function add_event($userid, $event_name, $start_date, $end_date, $event_details = null, $venue = null){
