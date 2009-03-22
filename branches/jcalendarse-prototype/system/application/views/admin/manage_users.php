f335a036f87e315e6bed3ad5b8a4c24a7231d464
cedualino200 <cedualino200@1b50f3e4-f447-11dd-a320-db7d8e7a638f>
2009-03-22 04:51:36 +0000

    user search. ang bobo ng evaluate ng CodeIgniter ng rules >:(

--- branches/jcalendarse-prototype/system/application/views/admin/manage_users.php
+++ branches/jcalendarse-prototype/system/application/views/admin/manage_users.php
@@ -1,4 +1,17 @@
-<?php $tmpl = array ( 'table_open'  => '<table id="events" border="1" cellpadding="4">' );
+<?php
+echo validation_errors();
+echo form_open('admin/manage_users');
+echo form_fieldset('Search Users');
+echo form_label('User ID: ', 'userid').form_input('userid', set_value('userid')?set_value('userid'):$userid).br(1);
+echo form_label('Student Number: ', 'studentid').form_input('studentid', set_value('studentid')?set_value('studentid'):$studentid).br(1);
+echo form_label('Login: ', 'login').form_input('login', set_value('login')?set_value('login'):$login).br(1);
+echo form_label('Lastname: ', 'lastname').form_input('lastname', set_value('lastname')?set_value('lastname'):$lastname).br(1);
+echo form_label('Firstname: ', 'firstname').form_input('firstname', set_value('firstname')?set_value('firstname'):$firstname).br(1);
+echo form_label('Middlename: ', 'middlename').form_input('middlename', set_value('middlename')?set_value('middlename'):$middlename).br(1);
+echo form_submit('submit', 'Search');
+echo form_fieldset_close();
+echo form_close();
+$tmpl = array ( 'table_open'  => '<table id="events" border="1" cellpadding="4">' );
 $this->table->set_template($tmpl);
 $this->table->set_heading('User ID','Student Number','Login','Name','Action');
 foreach($users as $user){
